4b37b1781b3a415186530ead4569ee619254fe35
m1kky <rysla@DESKTOP-RKLCQQR>
2024-07-28 21:40:04 +0300

    dadsaas

--- Source/MyProject2/food.h
+++ Source/MyProject2/food.h
@@ -27,6 +27,8 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	void RespawnFood();
+
 	virtual void Interact(AActor* Interactor, bool bIsHead, float SpeedCoeff) override;
 
 };
